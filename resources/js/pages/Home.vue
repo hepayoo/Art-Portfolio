03ee86492f888abee3e9d4abf011e6e99ebefff4
hepayo <elgotbihiba2005@gmail.com>
2024-07-25 17:04:46 +0100

    intgrating Art page and about and Contact us

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1>home page</h1>
+    
 
     <header class="header">
         <div class="header-text">
@@ -12,19 +12,20 @@
         <h2 class="header-title">Latest Art Posts</h2>
         <section class="cards-blog latest-blog">
           <div class="card-blog-content">
-            <img src="C:\xampp\htdocs\Art\public\images\Ivan Aivazovsky Stormy Sea, Large oil painting reproduction.jpeg" alt="" />
+            <img src="\public\images\Ivan Aivazovsky Stormy Sea, Large oil painting reproduction.jpeg" alt="" />
             <p>
               2 hours ago
               <span style="float: right">Written By Alphayo Wakarindi</span>
             </p>
             <h4 style="font-weight: bolder">
-              <a href="single-blog.html">Ivan Aivazovsky Stormy Sea, Large oil painting</a
+              <a href="single-blog.html"></a
               >
+              <router-link :to="{name:'SingleBlog' ,params:{slug:'Ivan Aivazovsky Stormy Sea, Large oil painting'}}">Ivan Aivazovsky Stormy Sea, Large oil painting</router-link>
             </h4>
           </div>
 
           <div class="card-blog-content">
-            <img src="C:\xampp\htdocs\Art\public\images\London Painting ACEO Original Watercolor England Sketch Miniature Artwork by Anamustudio - Etsy.jpeg" alt="" />
+            <img src="\public\images\London Painting ACEO Original Watercolor England Sketch Miniature Artwork by Anamustudio - Etsy.jpeg" alt="" />
             <p>
               23 hours ago
               <span style="float: right">Written By Alphayo Wakarindi</span>
@@ -35,7 +36,7 @@
           </div>
 
           <div class="card-blog-content">
-            <img src="C:\xampp\htdocs\Art\public\images\téléchargement (14).jpeg" alt="" />
+            <img src="\public\images\téléchargement (14).jpeg" alt="" />
             <p>
               2 days ago
               <span style="float: right">Written By Alphayo Wakarindi</span>
@@ -48,7 +49,7 @@
           </div>
 
           <div class="card-blog-content">
-            <img src="C:\xampp\htdocs\Art\public\images\A109.jpeg" alt="" />
+            <img src="\public\images\A109.jpeg" alt="" />
             <p>
               3 days ago
               <span style="float: right">Written By Alphayo Wakarindi</span>
--- resources/js/pages/SingleBlog.vue
+++ resources/js/pages/SingleBlog.vue
@@ -1,3 +1,56 @@
 <template>
     <h1>Art page</h1>
+  
+      <section class="single-blog-post">
+        <h1> Ivan Aivazovsky Stormy Sea, Large oil painting</h1>
+
+        <p class="time-and-author">
+          2 hours ago
+          <span>Written By Alphayo Wakarindi</span>
+        </p>
+
+        <div class="single-blog-post-ContentImage" data-aos="fade-left">
+          <img src="\public\images\Ivan Aivazovsky Stormy Sea, Large oil painting reproduction.jpeg" alt="" />
+        </div>
+
+        <div class="about-text">
+          <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem veritatis architecto voluptatibus quisquam minus? Cumque provident beatae quis ipsum. Iure natus harum veniam quasi atque delectus culpa repellendus cupiditate ut?
+            <br><br>
+            
+        
+          </p>
+        </div>
+      </section>
+      <section class="recommended">
+        <p>Related</p>
+        <div class="recommended-cards">
+          <a href="">
+            <div class="recommended-card">
+              <img src="\public\images\Art Nouveau Floral.jpeg" alt="" loading="lazy" />
+              <h4>
+                Modern Art
+              </h4>
+            </div>
+          </a>
+          <a href="">
+            <div class="recommended-card">
+              <img src="\public\images\téléchargement (14).jpeg" alt="" loading="lazy" />
+              <h4>
+                Classic Van-gogh
+              </h4>
+            </div>
+          </a>
+          
+          <a href="">
+            <div class="recommended-card">
+              <img src="\public\images\London Painting ACEO Original Watercolor England Sketch Miniature Artwork by Anamustudio - Etsy.jpeg" alt="" loading="lazy" />
+              <h4>
+                London-Sketch
+              </h4>
+            </div>
+          </a>
+
+        </div>
+      </section>
 </template>
