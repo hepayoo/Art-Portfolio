00d9ddd01831384d34db22e203f5ecfacd900273
hepayo <elgotbihiba2005@gmail.com>
2024-07-25 10:39:13 +0100

    art-portfolio

---
+++ resources/js/App.vue
@@ -0,0 +1,55 @@
+<template>
+    <h1><div id="wrapper">
+      <!-- header -->
+   
+
+      <!-- sidebar -->
+      <div class="sidebar">
+        <span class="closeButton">&times;</span>
+        <p class="brand-title"><a href="">Art</a></p>
+
+        <div class="side-links">
+          <ul>
+            <li><a class="active" href="index.html">Home</a></li>
+            <li><a href="blog.html">Art</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+        </div>
+
+        <!-- sidebar footer -->
+        <footer class="sidebar-footer">
+          <div>
+            <a href=""><i class="fab fa-facebook-f"></i></a>
+            <a href=""><i class="fab fa-instagram"></i></a>
+            <a href=""><i class="fab fa-twitter"></i></a>
+          </div>
+
+          
+        </footer>
+      </div>
+      <!-- Menu Button -->
+      <div class="menuButton">
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+      </div>
+      <!-- main -->
+      <main class="container">
+      <!--render-components on the page visited like :home ,about ,contact us-->
+      </main>
+
+      <!-- Main footer -->
+      <footer class="main-footer">
+        <div>
+          <a href=""><i class="fab fa-facebook-f"></i></a>
+          <a href=""><i class="fab fa-instagram"></i></a>
+          <a href=""><i class="fab fa-twitter"></i></a>
+        </div>
+        
+      </footer>
+    </div></h1>
+</template>
+<style>
+
+</style>
---
+++ resources/js/pages/Blog.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>blog page</h1>
+</template>
